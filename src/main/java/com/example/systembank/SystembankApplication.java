48639fccb2f81da82e953eee8a2f92e9769ae087
Matheus Faias <faiaxx@MacBook-Air-de-Matheus.local>
2026-03-26 19:36:47 -0300

    feat: primeiro commit do projeto Sistema Bancario

---
+++ src/main/java/com/example/systembank/SystembankApplication.java
@@ -0,0 +1,13 @@
+package com.example.systembank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystembankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystembankApplication.class, args);
+	}
+
+}
